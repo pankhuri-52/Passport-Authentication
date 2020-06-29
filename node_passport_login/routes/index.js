7b9b06cc72dbd4d10603b46bf89be6b1caab0097
Pankhuri <pankhuritrikha@gmail.com>
2020-06-29 11:41:17 +0530

    Issue Fixed and rendering of ejs file welcome

--- node_passport_login/routes/index.js
+++ node_passport_login/routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res) => {
-    res.send('Welcome');
-});
+router.get('/',(req,res) =>
+    res.render('welcome') // view we want to render
+);
 
 module.exports = router;
