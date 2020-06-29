65dfd80b4ca565d2990ffa2d3b824bbc6fb1397e
Pankhuri <pankhuritrikha@gmail.com>
2020-06-29 12:43:39 +0530

    Done with the user schema

--- node_passport_login/app.js
+++ node_passport_login/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//DB config
+const db = require('./config/keys').MongoURI;
+
+//connect to Mongo
+mongoose.connect(db,{useNewUrlParser : true, useUnifiedTopology : true})
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
 
 //ejs
 app.use(expressLayouts);
