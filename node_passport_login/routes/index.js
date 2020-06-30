370e33614070b884723b45df139334ccdf50566e
Pankhuri <pankhuritrikha@gmail.com>
2020-06-30 13:25:30 +0530

    Implemented Dashboard

--- node_passport_login/routes/index.js
+++ node_passport_login/routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+// Welcome page
 router.get('/',(req,res) =>
     res.render('welcome') // view we want to render
+);
+
+// Dashboard page
+router.get('/dashboard',(req,res) =>
+    res.render('dashboard') // view we want to render
 );
 
 module.exports = router;
