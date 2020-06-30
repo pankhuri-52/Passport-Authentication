e130f0b9050e58654132f08af86205e2c66fa5d4
Pankhuri <pankhuritrikha@gmail.com>
2020-06-30 10:51:56 +0530

    Users data entered into database

--- node_passport_login/routes/users.js
+++ node_passport_login/routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+// User model
+const User = require('../models/User');
 
 //Login Page
 router.get('/login',(req,res) => {
@@ -39,9 +43,46 @@ router.post('/register',(req,res) => {
             email,
             password,
             password2
-        })
+        });
     } else {
-        res.send('pass');
+       //Validation passed
+        User.findOne({ email : email })
+           .then(user => {
+               if(user) {
+                   //User exists
+                   errors.push({ msg : 'Email is already registered' });
+                   res.render('register', {
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2
+                });
+               } else {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password
+                    });
+
+                   // Hash password (generate a salt so that we can create a hash)
+                   // 10 is salt rounds
+                   bcrypt.genSalt(10, (err, salt) => 
+                        bcrypt.hash(newUser.password, salt, (err,hash) => {
+                            if(err) throw err;
+
+                            //set password to hashed
+                            newUser.password = hash;
+
+                            //Save User
+                            newUser.save()
+                             .then(user => {
+                                 res.redirect('/users/login');
+                             })
+                             .catch(err => console.log(err))
+                  }))
+               }
+           });
     }
 });
 
