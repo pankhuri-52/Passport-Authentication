b704da33e2b31260bb24fcee2b04941079cb1557
Pankhuri <pankhuritrikha@gmail.com>
2020-06-30 13:11:28 +0530

    DOne with the login part

--- node_passport_login/routes/users.js
+++ node_passport_login/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 // User model
 const User = require('../models/User');
@@ -85,6 +86,15 @@ router.post('/register',(req,res) => {
                }
            });
     }
+});
+
+//Login Handle
+router.post('/login',(req,res,next) => {
+    passport.authenticate('local',{
+        successRedirect : '/dashboard',
+        failureRedirect : '/users/login',
+        failureFlash : true
+    })(req,res,next);
 });
 
 module.exports = router;
